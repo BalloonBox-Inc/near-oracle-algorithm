bfbbc77d932f4ba302f07f0cbb4cd683e382774a
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-07-26 15:02:24 -0700

    Update database.py

--- support/database.py
+++ support/database.py
@@ -5,9 +5,7 @@ import psycopg2
 load_dotenv()
 
 
-DB_URI = getenv('DATABASE_URL').replace(
-    '@', ':').replace('/', ':').split(':')[3:]
-
+DB_URI = getenv('DATABASE_URL').replace('@', ':').replace('/', ':').split(':')[3:]
 USER, PASSWORD, HOST, PORT, DATABASE = [DB_URI[i] for i in tuple(range(5))]
 
 
@@ -25,19 +23,16 @@ def add_row_to_table(tablename, obj):
         with psycopg2.connect(user=USER, password=PASSWORD, host=HOST, port=PORT, database=DATABASE) as conn:
             with conn.cursor() as cursor:
                 # create table if it does not exist
-                cursor.execute(
-                    f'CREATE TABLE IF NOT EXISTS {tablename} ({metadata})')
+                cursor.execute(f'CREATE TABLE IF NOT EXISTS {tablename} ({metadata})')
                 conn.commit()
 
                 # add column if it is not in table
                 for column_name in list(obj.keys()):
-                    cursor.execute(
-                        f'ALTER TABLE {tablename} ADD COLUMN IF NOT EXISTS {column_name} VARCHAR(255)')
+                    cursor.execute(f'ALTER TABLE {tablename} ADD COLUMN IF NOT EXISTS {column_name} VARCHAR(255)')
                     conn.commit()
 
                 # add values to table as a row
-                cursor.execute(
-                    f'INSERT INTO {tablename} ({columns}) VALUES ({values})')
+                cursor.execute(f'INSERT INTO {tablename} ({columns}) VALUES ({values})')
                 conn.commit()
 
     except (Exception, psycopg2.Error) as e:
