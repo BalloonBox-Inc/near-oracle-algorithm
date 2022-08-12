c1fba27b07c2b3dc60e43f0d2e28c82de07f9df8
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-08-12 16:06:30 -0700

    Update database.py

--- support/database.py
+++ support/database.py
@@ -5,7 +5,11 @@ from os import getenv
 load_dotenv()
 
 
-engine = create_engine(getenv('DATABASE_URL'))
+DATABASE_URL = getenv('DATABASE_URL')
+if 'postgresql' not in DATABASE_URL:
+    DATABASE_URL = DATABASE_URL.replace('postgres', 'postgresql')
+
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
