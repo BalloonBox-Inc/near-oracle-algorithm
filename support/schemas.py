595a2fdc96ed156ea99726578dfeedfb0f609679
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-08-23 11:13:57 -0700

    Update schemas.py

--- support/schemas.py
+++ support/schemas.py
@@ -3,35 +3,55 @@ from typing import Optional
 
 
 # http request classes
-class Plaid_Item(BaseModel):
+class CoinmarketCap_Access(BaseModel):
+    coinmarketcap_key: str
+
+
+class Coinbase_Access(BaseModel):
+    coinbase_access_token: str
+    coinbase_refresh_token: str
+
+
+class Covalent_Access(BaseModel):
+    eth_address: str
+    covalent_key: str
+
+
+class Plaid_Access(BaseModel):
     plaid_access_token: str
     plaid_client_id: str
     plaid_client_secret: str
-    coinmarketcap_key: str
+
+
+class Loan_Item(BaseModel):
     loan_request: int
 
 
-class Coinbase_Item(BaseModel):
-    coinbase_access_token: str
-    coinbase_refresh_token: str
-    coinmarketcap_key: str
-    loan_request: int
+class Coinbase_Item(
+        Coinbase_Access, CoinmarketCap_Access, Loan_Item):
+    pass
 
 
-class Covalent_Item(BaseModel):
-    eth_address: str
-    covalent_key: str
-    coinmarketcap_key: str
-    loan_request: int
+class Covalent_Item(
+        Covalent_Access, CoinmarketCap_Access, Loan_Item):
+    pass
+
+
+class Plaid_Item(
+        Plaid_Access, CoinmarketCap_Access, Loan_Item):
+    pass
 
 
 class KYC_Item(BaseModel):
     chosen_validator: str
+    coinmarketcap_key: str
+
     plaid_access_token: Optional[str]
     plaid_client_id: Optional[str]
     plaid_client_secret: Optional[str]
+
     coinbase_access_token: Optional[str]
     coinbase_refresh_token: Optional[str]
-    coinmarketcap_key: Optional[str]
+
     eth_address: Optional[str]
     covalent_key: Optional[str]
